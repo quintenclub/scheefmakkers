ded45708272dbbbfc8a07e94ca84ab8d3bc95aac
Quinten Clause <quintenclause@hotmail.com>
2024-02-22 13:49:05 +0100

    Add timelapse

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 document.querySelectorAll('a[href^="#"]').forEach(el => {
-  el.addEventListener('click', e => {
-    e.preventDefault();
-    document.querySelector(e.target.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
+    el.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(e.target.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
-  });
+});
+let nodes = document.querySelectorAll('.footernavfixed');
+if (window.scrollY < 128 && document.body.offsetHeight > window.innerHeight) {
+    nodes.forEach(node => {
+        node.classList.add('footernavfixed--hidden');
+    });
+}
+window.addEventListener('scroll', () => {
+    nodes.forEach(node => {
+        if (window.scrollY < 128) {
+            node.classList.add('footernavfixed--hidden');
+        } else {
+            node.classList.remove('footernavfixed--hidden');
+        }
+    });
 });
